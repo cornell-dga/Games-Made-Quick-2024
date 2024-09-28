73821cea57406d224d7efe11bbe049dc052e4a47
LWDaniels <lawrencedanielsjr@gmail.com>
2024-09-28 16:55:09 -0400

    Basic Player Controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D myRB;
+    public float laneSwapSpeed;
+    public float percentDistanceFromTop;
+    public int maxLaserCharges;
+    public int startLane;
+    private float[] laneHeights;
+    private int laserCharges;
+    private int laneTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laneTarget= startLane;
+        laserCharges = 0;
+        laneHeights = new float[5];
+        float top = 2.5f;
+        for (int i = 0; i < 5; i++)
+        {
+            laneHeights[i] = top - i*1.25f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, laneHeights[laneTarget]), laneSwapSpeed);
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            // move down
+            if (laneTarget == 4) return;
+            laneTarget++;
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            // move up
+            if (laneTarget == 0) return;
+            laneTarget--;
+        }
+    }
+}
